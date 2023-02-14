786a205d6b33ad269d8cb7b0102ce09cad9d5718
Daureo Silva <daureocosta@gmail.com>
2023-02-14 15:59:18 -0300

    Used useState to get TextInput values and make calculations

--- Aula01/App.js
+++ Aula01/App.js
@@ -1,25 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { TextInput } from 'react-native-web';
+import { TextInput } from 'react-native';
 
 export default function App() {
+
+
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [imc, setIMC] = useState('');
+
+  const CalcIMC = () => {
+    const resultado = Number(peso)/(Number(altura)*Number(altura));
+    setIMC(resultado.toString());
+    //console.log(imc)
+  }
+
   return (
     <View style={styles.container}>
       <Text>Cálculo de IMC</Text>
 
       <Text>Peso</Text>
       <TextInput 
+      keyboardType="numeric"
+      value={peso}
+      onChangeText={setPeso}
       style={styles.caixaTexto}
       
       />
       <Text>Altura</Text>
       <TextInput 
+      keyboardType="numeric"
+      value={altura}
+      onChangeText={setAltura}
       style={styles.caixaTexto}
+      
       />
-    <TextInput style={{height:40}}/>
-      <Button title="Calcular" onPress={() => {
-        CalcIMC()
-      }} />
+    <TextInput 
+    value={imc}
+    style={{height:40}}
+    
+    />
+
+      <Button title="Calcular" onPress={()=>{CalcIMC()}} />
       
       <StatusBar style="auto" />
     </View>
@@ -34,6 +57,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   caixaTexto: {
+    //flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 100,
     height: 30,
     borderColor: 'gray',
     borderWidth: 1,
@@ -41,6 +69,3 @@ const styles = StyleSheet.create({
  
 });
 
-const CalcIMC = (altura, peso) => {
-  return altura/(peso*peso)
-}
